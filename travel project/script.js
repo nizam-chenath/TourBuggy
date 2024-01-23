4053ee952c137ffbad8ed19f5c659201de906550
nizam-chenath <nizamstudymajliz@gmail.com>
2024-01-23 07:40:43 +0530

    gallery sectoin added

---
+++ travel project/script.js
@@ -0,0 +1,76 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', function () {
+  const animatedTextElements = document.querySelectorAll('.animated-text');
+
+  function fadeInElements() {
+    animatedTextElements.forEach((element, index) => {
+      setTimeout(() => {
+        element.style.opacity = 1;
+      }, index * 2000); // Adjust the delay (in milliseconds) as needed
+    });
+  }
+
+  fadeInElements();
+});
+
+let backBtn = document.getElementById("backBtn");
+let nextBtn = document.getElementById("nextBtn");
+let scrollView = document.querySelector(".gallery")
+
+
+scrollView.addEventListener("wheel", (event) =>{
+  event.preventDefault()
+  scrollView.scrollLeft += event.deltaY;
+})
+scrollView.addEventListener('scroll', () => {
+  let index = Math.round(scrollView.scrollLeft / scrollView.offsetWidth);
+  updateActiveDot(index);
+});
+
+nextBtn.addEventListener("click", () => {
+  scrollView.style.scrollBehavior = "smooth";
+  let scrollAmount = scrollView.offsetWidth;
+  scrollView.scrollLeft += scrollAmount;
+});
+
+backBtn.addEventListener("click", () => {
+  scrollView.style.scrollBehavior = "smooth";
+  let scrollAmount = scrollView.offsetWidth;
+  scrollView.scrollLeft -= scrollAmount;
+});
+
+
+// dot nvigation
+const images = document.querySelectorAll(".gallery img");
+const dotContainer = document.querySelector(".dot-container");
+
+// Create dots
+// images.forEach((_, index) => {
+//     const dot = document.createElement("span");
+//     dot.classList.add("dot");
+//     if(index === 0) dot.classList.add("active");
+//     dotContainer.appendChild(dot);
+// });
+
+// // Update active dot
+function updateActiveDot(index) {
+    document.querySelectorAll('.dot').forEach(dot => {
+        dot.classList.remove('active');
+    });
+    document.querySelectorAll('.dot')[index].classList.add('active');
+}
+
+images.forEach((img, index) => {
+  const dot = document.createElement("span");
+  dot.classList.add("dot");
+  if (index === 0) dot.classList.add("active");
+
+  // Click event for each dot
+  dot.addEventListener('click', () => {
+      scrollView.scrollLeft = img.offsetLeft;
+      updateActiveDot(index);
+  });
+
+  dotContainer.appendChild(dot);
+});
